00e5ba7fe5a9179f7ea886faa9d5980a953f10a9
Sachin Singh <akasachin45@gmail.com>
2025-08-13 01:45:24 +0530

    Programe_1

---
+++ Programe_1.py
@@ -0,0 +1,7 @@
+# Python to that checks if the number entered by the user is even or odd
+
+number = int(input("Enter a Number:"))
+if number % 2 == 0:
+    print("The Number is Even")
+else:
+    print("The Number is Odd")
